081b664b1511d87fa505fad1f6f35c4786362742
Gabriel Guislotti <gabrielguislotti@hotmail.com>
2025-04-22 21:08:29 -0300

    Implementando LocacaoService e LocacaoListComponent para listar as locacoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocacaoListComponent } from './locacao/locacao-list/locacao-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocacaoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/locacao/locacao-list/locacao-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-locacao-list',
+  standalone: false,
+  templateUrl: './locacao-list.component.html',
+  styleUrl: './locacao-list.component.css'
+})
+export class LocacaoListComponent {
+
+}
